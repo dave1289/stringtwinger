55292395f905d731676eb5b8603fade06d043573
dave-home-2 <davemcelhaney@gmail.com>
2023-05-24 18:11:05 -0400

    setting up communication backend

---
+++ server/app.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/** Express app for jobly. */
+
+const express = require("express");
+const helpers = require("./helpers")
+
+const { NotFoundError } = require("./expressError");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/chords", (req, res) => {
+   const chords = helpers.pullChords()
+   console.log(chords)
+   res.json({data: `${chords}`})
+})
+
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  const status = err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
+
+module.exports = app;
---
+++ server/helpers.js
@@ -0,0 +1,71 @@
+const axios = require('axios')
+
+const BASE_URL = 'https://api.uberchord.com/v1/chords/'
+
+const keys = {
+   A: ['A', 'B_m', 'Db', 'D', 'E', 'Gb_m', 'Ab_dim'],
+   B: ['B', 'Db_m', 'Eb_m', 'Gb', 'Ab_m', 'Bb_dim'],
+   C: ['C', 'Dm', 'E_m', 'F', 'G', 'A_m', 'B_dim'],
+   D: ['D', 'E_m', 'Gb_m', 'G', 'A', 'B_m', 'Db_dim'],
+   E: ['E', 'Gb_m', 'Ab_m', 'A', 'B', 'Db_m', 'Cb_dim'],
+   F: ['F', 'G_m', 'A_m', 'Bb', 'C', 'D_m', 'E_dim'],
+   G: ['G', 'A_m', 'B_m', 'C', 'D', 'E_m', 'Gb_dim'],
+   A_m: ['A_m', 'B_dim', 'C', 'Dm', 'E_m', 'F', 'G',],
+   B_m: ['B_m', 'Db_dim', 'D', 'E_m', 'Gb_m', 'G', 'A',],
+   C_m: ['C_m', 'D_dim', 'Eb', 'F_m', 'G_m', 'Ab', 'Bb'],
+   D_m: ['D_m', 'E_dim', 'F', 'G_m', 'A_m', 'Bb', 'C',],
+   E_m: ['E_m', 'Gb_dim', 'G', 'A_m', 'B_m', 'C', 'D',],
+   F_m: ['F_m', 'G_dim', 'Ab', 'Bb_m', 'C_m', 'Db', 'Eb'],
+   G_m: ['G_m', 'A_dim', 'Bb', 'C_m', 'D_m', 'Eb', 'F']
+}
+
+const progressions = [
+   [1, 3, 5],
+   [1, 4, 6, 5],
+   [2, 5, 1],
+   [1, 6, 2, 5],
+   [1, 5, 6, 4],
+   [1, 4, 6, 5],
+   [1, 3, 4, 5],
+   [1, 4, 1, 5],
+   [1, 4, 2, 5]
+]
+
+// returns random key signature from object
+function getRandomKeySig(obj) {
+   const keys = Object.keys(obj);
+   return keys[Math.floor(Math.random() * keys.length)];
+}
+
+//  returns random chord progression from array
+function getRandomProg() {
+   return progressions[Math.floor(Math.random() * progressions.length)]
+}
+
+//  uses nums in progression to pull chords from key signature
+const ChordProgTest = () => {
+   const progression = getRandomProg()
+   const key = getRandomKeySig(keys)
+   const chords = []
+   // numbers in progression pulled from key signature
+   for (let num of progression) {
+      chords.push(keys[key][num - 1])
+   }
+   var result = chords.map(function(val) {
+      return val;
+    }).join(',');
+    console.log(`${BASE_URL}${result}`)
+   return `${BASE_URL}${result}`
+}
+
+async function pullChords() {
+   const res = await axios.get(ChordProgTest())
+   return res.data
+}
+
+module.exports = {
+   ChordProgTest,
+   keys,
+   progressions,
+   pullChords
+};
